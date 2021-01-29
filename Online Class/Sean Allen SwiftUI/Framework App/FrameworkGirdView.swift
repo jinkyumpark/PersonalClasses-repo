859b8ac9a785ffd87583ed1d79f5b5096ef83aec
jinkyumpark <61900235+jinkyumpark@users.noreply.github.com>
2021-01-30 04:10:55 +0900

    Create FrameworkGirdView.swift

---
+++ Online Class/Sean Allen SwiftUI/Framework App/FrameworkGirdView.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+struct FrameworkGridView: View {
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                LazyVGrid(columns: columns){
+                    ForEach(MockData.frameworks){ framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("Apple Frameworks")
+
+        }
+    }
+    
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+            .preferredColorScheme(.dark)
+    }
+}
+ 
+struct FrameworkTitleView: View {
+
+    let framework: Framework
+    
+    var body: some View{
+        VStack{
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 80, height: 80)
+                
+            
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+                
+        }
+        .padding()
+    }
+}
