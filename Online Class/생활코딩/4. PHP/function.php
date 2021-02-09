34c34197f297c882f01bb93c9c5229feae2b3ad1
jinkyumpark <61900235+jinkyumpark@users.noreply.github.com>
2021-02-09 17:52:56 +0900

    Create function.php

---
+++ Online Class/생활코딩/4. PHP/function.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+
+	<body>
+		<h1>function</h1>
+
+		<?php
+		$str = "Love you
+
+		But...";
+		echo $str;
+		?>
+
+		<?php
+		echo nl2br($str);
+		?>
+
+	</body>
+
+</html>
