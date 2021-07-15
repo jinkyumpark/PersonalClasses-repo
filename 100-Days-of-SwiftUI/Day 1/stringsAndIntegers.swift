85db59eaa8b49c4446415fbc62ba512fe9bebfe1
jinkyumpark <61900235+jinkyumpark@users.noreply.github.com>
2021-07-15 14:50:00 +0900

    Create stringsAndIntegers.swift

---
+++ 100-Days-of-SwiftUI/Day 1/stringsAndIntegers.swift
@@ -0,0 +1,8 @@
+//Type Safe Language : Every variable has specific type
+//Why? So that we don't make mistakes with our data
+
+var str = "Hello, playground"
+var age = 38
+
+//Thousands Seperator
+var population = 8_000_000
