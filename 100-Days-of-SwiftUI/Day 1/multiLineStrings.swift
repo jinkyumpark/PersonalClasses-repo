30b03273686862ca16f389538d5822075075590d
jinkyumpark <61900235+jinkyumpark@users.noreply.github.com>
2021-07-15 14:51:58 +0900

    Create multiLineStrings.swift

---
+++ 100-Days-of-SwiftUI/Day 1/multiLineStrings.swift
@@ -0,0 +1,14 @@
+var str1 = """
+This goes
+over multiple
+lines
+"""
+//Must be in their own lines
+
+//In order to insert line break, we must use \
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
